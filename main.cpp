103b3d31bd6480edd11a3cae118b6a4b012a7ebc
Chris Bonne <christiaanbonne@live.nl>
2026-04-19 16:49:13 +0000

    fix main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+
+#include "cudafunctions/benchmark.h"
+
+int main() {
+
+
+    return 0;
+}
