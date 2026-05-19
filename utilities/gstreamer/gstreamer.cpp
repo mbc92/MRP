5f46db13e7d2f151e48ffafc34d4457f64ba32db
Chris Bonne <christiaanbonne@live.nl>
2026-05-19 18:48:32 +0000

    add gstreamer wrapper tempalte

--- utilities/gstreamer/gstreamer.cpp
+++ utilities/gstreamer/gstreamer.cpp
@@ -2,3 +2,17 @@
 
 // GStreamer implementation
 // Add your GStreamer functionality here
+
+Gstreamer::Gstreamer()
+    :_server(std::make_unique<GstRTSPServer>()),
+     _mounts(gst_rtsp_server_get_mount_points(_server.get())),
+     _factory(gst_rtsp_media_factory_new())
+{
+    
+}
+
+Gstreamer::~Gstreamer()
+{
+    // Destructor implementation
+}
+
--- utilities/gstreamer/gstreamer.h
+++ utilities/gstreamer/gstreamer.h
@@ -1,4 +1,19 @@
 #pragma once
 
 #include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
+#include <memory>
 
+// Wrapper class for GStreamer functionality
+class Gstreamer
+{
+public:
+    Gstreamer();
+    ~Gstreamer();
+
+private:
+
+    std::unique_ptr<GstRTSPServer> _server;
+    std::unique_ptr<GstRTSPMountPoints> _mounts;
+    std::unique_ptr<GstRTSPMediaFactory> _factory;
+};
